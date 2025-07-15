6be1adc0c297b86c038a90fb616c63dd76f7403a
Levi H <levih@Levi-MacBook-Pro.local>
2025-07-15 11:52:47 +0900

    fix: `process_sheet_request` 함수에서 `load_sheet_to_bigquery`를 호출하지 않던 버그 수정

--- main.py
+++ main.py
@@ -166,6 +166,7 @@ def process_sheet_request():
     
     try:
         print(f"[{config_key}] 작업 시작...", flush=True)
+        load_sheet_to_bigquery(config_key)
         print(f"[{config_key}] 작업 성공적으로 완료.", flush=True)
         return Response(f"Success: Job for config_key '{config_key}' completed.", status=200)
     except Exception as e:
